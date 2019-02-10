4ecc4f6e7a125dd7e0193422f8b7970aa8b9eda3
leifo <lop@gmx.de>
2019-02-10 13:05:09 +0100

    screenmodes example update

--- demo/screenmodes/src/main.c
+++ demo/screenmodes/src/main.c
@@ -39,27 +39,26 @@ void updateDemo(int time)
  *   7  |   Y   |   N   | 320x200x6, (Amiga: few pixel errors, stemming from GIF?) (PC: horizontal stripes)
  *   8  |   Y   |   Y   | 320x180x8
  *   9  |   Y   |   Y   | 320x90x8
- *  10  |   Y   |   N   | 160x90x8 (Y/2 on PC)
+ *  10  |   Y   |   Y   | 160x90x8 
  *  11  |   Y   |   Y   | 160x90x18
- *  12  |   Y   |   N   | 640x180x8 (just horizontal stripes on PC)
- *  13  |   Y   |   N   | 640x360x8 (just vertical striped on PC)
+ *  12  |   Y   |   Y   | 640x180x8
+ *  13  |   Y   |   Y   | 640x360x8
  *  14  |   Y   |   Y   | 320x180x6 w. saturation
  *  15  |   N   |   N   | 320x180x5 (upper bits), wrong colours (due to GIF?)
  *  16  |   Y   |   Y   | 320x180x8 w. copper colours (small black spots on Amiga due to non-init)
- *  17  |   Y   |  red  | 220x180x15 from 24-bit
- *  18  |   Y   |  red  | 220x180x15 from 18-bit
- *  19  |   Y   |  red  | 220x90x15 from 18-bit
- *  20  |   Y   |  red  | 220x180x18 from 24-bit
+ *  17  |   Y   |   Y   | 220x180x15 from 24-bit
+ *  18  |   Y   |   Y   | 220x180x15 from 18-bit
+ *  19  |   Y   |   Y   | 220x90x15 from 18-bit
+ *  20  |   Y   |   Y   | 220x180x18 from 24-bit
  *  21  |   Y   |   Y   | 220x180x18 from 18-bit
  *  22  |   Y   |   Y   | 220x90x18 from 18-bit
- *  23  |   Y   |  red  | 220x180x12 from 18-bit
- *  24  |   N   |   N   | 320x180x5 OCS, wrong colours (due to GIF?)
+ *  23  |   Y   |   Y   | 220x180x12 from 18-bit
+ *  24  |   N   |   Y   | 320x180x5 OCS, wrong colours (due to GIF?)
  *  25  |   N   |   Y?  | 320x180x5 OCS w. copper colours, wrong colours (due to GIF?, black spots on Amiga due to non-init)
  * 
  * */
 
-// working perfectly on Amiga and PC: 8, 9, 11, 14, 16, 21, 22
-// 7 out of 25
+// working perfectly on Amiga and PC: 8-14, 16-23
 
 int oldpart= -1;
 int pic= 8;
